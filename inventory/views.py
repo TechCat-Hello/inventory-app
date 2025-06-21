c2d70de6c9225f3230082cf03eda20a3570acef0
TechCat <a.m.techcat@gmail.com>
2025-06-21 09:55:47 +0900

    Collect static files including NotoSansJP font for PDF generation

--- inventory/views.py
+++ inventory/views.py
@@ -473,7 +473,7 @@ def export_rentals_pdf(request):
 
     # テンプレートをHTMLに変換
     html_string = render_to_string('inventory/rental_history_pdf.html', context)
-    html = HTML(string=html_string, base_url=request.build_absolute_uri('/'))
+    html = HTML(string=html_string, base_url=request.build_absolute_uri())
 
     pdf = html.write_pdf()
 
